988ee702d21ce913094c36fd4bc80b6badd589d0
r1411 <roma00055@gmail.com>
2022-12-02 10:12:39 +0300

    Просмотр таблиц, начата карточка артиста

--- dance_db_app/urls.py
+++ dance_db_app/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('artist/<int:artist_id>/', views.view_artist, name="view_artist"),
     path('track/<int:track_id>/', views.view_track, name="view_track"),
     path('dance/<int:dance_id>/', views.view_dance, name="view_dance"),
+    path('artists/', views.view_artists, name="view_all_artists"),
+    path('tracks/', views.view_tracks, name="view_all_tracks"),
+    path('dances/', views.view_dances, name="view_all_dances"),
 ]
--- dance_db_app/views.py
+++ dance_db_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.template import loader
 from django.http import HttpResponse, Http404
 from .models import Artist, Track, Dance
@@ -18,10 +18,32 @@ def index(request):
     return HttpResponse(template.render(context, request))
 
 def view_artist(request, artist_id):
-    return HttpResponse(f"You're looking at artist {artist_id}.")
+    artist = get_object_or_404(Artist, pk=artist_id)
+    return render(request, 'dance_db_app/artist.html', {'artist': artist})
 
 def view_track(request, track_id):
     return HttpResponse(f"You're looking at track {track_id}.")
 
 def view_dance(request, dance_id):
     return HttpResponse(f"You're looking at dance {dance_id}.")
+
+def view_artists(request):
+    template = loader.get_template('dance_db_app/artists.html')
+    context = {
+        'artists': Artist.objects.all()
+    }
+    return HttpResponse(template.render(context, request))
+
+def view_tracks(request):
+    template = loader.get_template('dance_db_app/tracks.html')
+    context = {
+        'tracks': Track.objects.all()
+    }
+    return HttpResponse(template.render(context, request))
+
+def view_dances(request):
+    template = loader.get_template('dance_db_app/dances.html')
+    context = {
+        'dances': Dance.objects.all()
+    }
+    return HttpResponse(template.render(context, request))
